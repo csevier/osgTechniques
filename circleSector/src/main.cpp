d270200cd62f58cb57be4debe75b1a936472ed1e
Cody Sevier <sevierit@gmail.com>
2020-06-10 00:19:08 -0500

    cleanup

--- circleSector/src/main.cpp
+++ circleSector/src/main.cpp
@@ -2,10 +2,17 @@
 #include <osg/Geode>
 #include <osg/Geometry>
 #include <osg/Vec3>
-#include <osgUtil/Tessellator>
-#include <cmath>
+#include <iostream>
 
-#include "logfilehandler.hpp"
+
+osg::Vec3 MidPointDivide(osg::Vec3 one, osg::Vec3 two)
+{
+    one.normalize();
+    two.normalize();
+    osg::Vec3 mid = (one + two) / 2;
+    mid.normalize();
+    return mid;
+}
 
 int main()
 {
@@ -14,14 +21,11 @@ int main()
     osg::Vec3 origin = osg::Vec3(0,0,0);
 
 
-    osg::Vec3 right = osg::Vec3(-4,0,0.5);
+    osg::Vec3 right = osg::Vec3(0,0,-1);
     right.normalize(); // now on the unit sphere.
 
     osg::Vec3 left = osg::Vec3(1,0,0);
     left.normalize(); // now on the unit sphere.
-
-    osg::Vec3 normalForCirclePlane = right ^ left; // cross product.
-    normalForCirclePlane.normalize();
 
     osg::Vec3 mid = (right + left) /2;
     mid.normalize();
@@ -39,42 +43,14 @@ int main()
     vertices->push_back(mid2);
     vertices->push_back(right); // finish
 
-
-    // circle equation
-    //float x = x0 + radius * (std::cos(phi)*std::cos(theta));
-    //float y = y0 + radius * (std::cos(phi)*std::sin(theta));
-    //float z = z0 + radius * std::sin(phi);
-
-    // generalize
-    //normalForCirclePlane.x()
-
-//    vertices->push_back(left);
     float angle = std::acos((left * right) / (right.length() * left.length())) * 180 / 3.14;
     std::cout << angle;
-//    float increment = 5;
-//    float startAngle = increment; // angles for points.
-//    float radius = 1;
-//    while(startAngle < angle)
-//    {
-//        float x = radius * std::cos(startAngle * 3.14/180);
-//        float y = radius * std::sin(startAngle* 3.14/180);
-//        osg::Vec3 point = osg::Vec3(x, 0, y);
-//        vertices->push_back(point);
-//        startAngle += increment;
-//    }
-
 
     osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array{};
     normals->push_back(osg::Vec3(0,-1,0));
-    //color->push_back(osg::Vec4(1,0,0,1));
 
     osg::ref_ptr<osg::Vec4Array> color = new osg::Vec4Array{};
     color->push_back(osg::Vec4(1,0,0,1));
-    color->push_back(osg::Vec4(0,1,0,1));
-    color->push_back(osg::Vec4(1,1,1,1));
-    color->push_back(osg::Vec4(1,1,1,1));
-    color->push_back(osg::Vec4(1,1,1,1));
-    color->push_back(osg::Vec4(0,0,1,1));
 
     osg::ref_ptr<osg::Geode> root = new osg::Geode{};
     osg::ref_ptr<osg::Geometry> geom = new osg::Geometry{};
@@ -83,7 +59,7 @@ int main()
     geom->setNormalArray(normals.get());
     geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
     geom->setColorArray(color.get());
-    geom->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    geom->setColorBinding(osg::Geometry::BIND_OVERALL);
     geom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::TRIANGLE_FAN,0,vertices->size()));
     root->addDrawable(geom.get());
     osgViewer::Viewer viewer;
